ebccb0f09fc58c41718960a6213f129b079c0b5a
ClecioSousa000 <cleciosouza015@gmail.com>
2025-02-25 20:19:42 -0300

    Feat: aula 35

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,13 @@
-import { knex as setupKnex } from "knex";
-
-export const knex = setupKnex({
-	client: "sqlite3",
+import { knex as setupKnex, type Knex } from "knex";
+export const config: Knex.Config = {
+	client: "sqlite",
 	connection: {
-		filename: "./tmp/app.db",
+		filename: "./db/app.db",
 	},
-});
+	useNullAsDefault: true,
+	migrations: {
+		extension: "ts",
+		directory: "./db/migrations",
+	},
+};
+export const knex = setupKnex(config);
