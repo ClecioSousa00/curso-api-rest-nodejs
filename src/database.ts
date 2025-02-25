b12acbf2ca4c403f9691aa0ba2b3ec9436fe2380
ClecioSousa000 <cleciosouza015@gmail.com>
2025-02-25 20:25:57 -0300

    Feat: aula 36

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,14 @@
+import "dotenv/config";
 import { knex as setupKnex, type Knex } from "knex";
+
+if (!process.env.DATABASE_URL) {
+	throw new Error("DATABASE");
+}
+
 export const config: Knex.Config = {
 	client: "sqlite",
 	connection: {
-		filename: "./db/app.db",
+		filename: process.env.DATABASE_URL,
 	},
 	useNullAsDefault: true,
 	migrations: {
